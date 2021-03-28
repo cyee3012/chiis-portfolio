51af88a580ffae69a5108071467e36e757a4d9bd
Chii Law <cyee3012@hotmail.co.uk>
2021-03-28 22:31:41 +0900

    added hover opacity change to imgs in projects

---
+++ source/assets/javascripts/clickPicture.js
@@ -0,0 +1,9 @@
+// Get the modal
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+// Get the <span> element that closes the modal
+// When the user clicks on <span> (x), close the modal
+
+const clickPicture = () => {
+
+
+}
