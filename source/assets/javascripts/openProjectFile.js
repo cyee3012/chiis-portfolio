7d9bb1a008f7138859c7d75c5814128a275b071c
Chii Law <cyee3012@hotmail.co.uk>
2021-03-29 02:03:07 +0900

    changed slideToggle to slideDown

--- source/assets/javascripts/openProjectFile.js
+++ source/assets/javascripts/openProjectFile.js
@@ -2,5 +2,5 @@ const header = document.querySelector('.project-header');
 
 header.addEventListener('click', () => {
   header.innerHTML = '<h2>&#x25BE; <i class="far fa-folder-open"></i> PROJECTS</h2>';
-  $('.project-list').slideToggle('slow');
+  $('.project-list').slideDown('slow');
 });
