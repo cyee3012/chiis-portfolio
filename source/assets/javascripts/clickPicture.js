082dc60fd7cd391d2af19b9bd8bcdd5b95b2c772
Chii Law <cyee3012@hotmail.co.uk>
2021-03-29 00:13:04 +0900

    image pops out on clicks

--- source/assets/javascripts/clickPicture.js
+++ source/assets/javascripts/clickPicture.js
@@ -2,8 +2,14 @@
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 // Get the <span> element that closes the modal
 // When the user clicks on <span> (x), close the modal
+const imgs = document.querySelectorAll('#project-img');
+const modal = document.querySelector('#myModal');
+const modalImg = document.querySelector('#img');
 
-const clickPicture = () => {
-
-
-}
+imgs.forEach((image) => {
+  image.addEventListener('click', (event) => {
+    const elem = event.target;
+    modal.style.display = "block";
+    modalImg.src = elem.src;
+  });
+});
