cb9754b674f9c51d17594995b5d0822da65948c4
Chii Law <cyee3012@hotmail.co.uk>
2021-03-29 22:12:25 +0900

    project header opens and closes like file directory. also changed favicon to match new theme

--- source/assets/javascripts/openProjectFile.js
+++ source/assets/javascripts/openProjectFile.js
@@ -1,6 +1,11 @@
 const header = document.querySelector('.project-header');
 
 header.addEventListener('click', () => {
-  header.innerHTML = '<h2>&#x25BE; <i class="far fa-folder-open"></i> PROJECTS</h2>';
-  $('.project-list').slideDown('slow');
+  if (header.innerHTML.includes('fa-folder-open')) {
+    header.innerHTML = '<h2>&#x25B8; <i class="far fa-folder"></i> PROJECTS</h2>';
+    $('.project-list').slideToggle('slow');
+  } else {
+    header.innerHTML = '<h2>&#x25BE; <i class="far fa-folder-open"></i> PROJECTS</h2>';
+    $('.project-list').slideToggle('slow');
+  }
 });
